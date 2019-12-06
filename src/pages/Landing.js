04be132cbb5e1ade6d6a651943b3ba3e305c8535
Alex Khoa Le <leduykhoa@hotmail.ca>
2019-12-06 11:54:03 -0500

    Landing page text changed slightly

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -18,7 +18,7 @@ function Landing() {
             <div className="jumbotron">
                 <div className="container">
                     <h1 className="display-4">Welcome to Infact!</h1>
-                    <p className="lead">The job board for contract and temporary work for Freelancers</p>
+                    <p className="lead">The contract and temporary work job board for Freelancers</p>
                 </div>
             </div>
             <SearchBar handleSearch={handleSearch} submitSearch={submitSearch}/>
