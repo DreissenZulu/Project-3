9f654303eb1d0be2211e1f2e19b4e36ba5410aca
Alex Khoa Le <leduykhoa@hotmail.ca>
2019-12-06 13:39:38 -0500

    Changed nav Links to NavLink, style changes when active

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,25 +1,26 @@
 import React from 'react';
 import Navbar from "react-bootstrap/Navbar";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
-function NavBar() {
+function NavBar(props) {
+  console.log(props)
   return (
     <Navbar sticky="top" bg="light" expand="md">
       <Link to="/" className="navbar-brand">Infact</Link>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <ul className="navbar-nav ml-auto">
-          <li className="nav-item active">
-            <Link to="/" className="nav-link">Home</Link>
+          <li className="nav-item">
+            <NavLink exact to="/" className="nav-link" activeClassName="active">Home</NavLink>
           </li>
           <li className="nav-item">
-            <Link to="/profile" className="nav-link">Profile</Link>
+            <NavLink to="/profile" className="nav-link" activeClassName="active">Profile</NavLink>
           </li>
           <li className="nav-item">
-            <Link to="/signup" className="nav-link">Sign Up</Link>
+            <NavLink to="/signup" className="nav-link" activeClassName="active">Sign Up</NavLink>
           </li>
           <li className="nav-item">
-            <Link to="/login" className="nav-link">Log In</Link>
+            <NavLink to="/login" className="nav-link" activeClassName="active">Log In</NavLink>
           </li>
         </ul>
       </Navbar.Collapse>
