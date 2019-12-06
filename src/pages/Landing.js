3b58de86ab9bd5485de5446a9a02253a3ec4669a
Alex Khoa Le <leduykhoa@hotmail.ca>
2019-12-05 20:55:40 -0500

    Added search bar handling. Imported search bar component

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
+import SearchBar from "../components/SearchBar";
 
 function Landing() {
+    const [ search, setSearch ] = useState("")
+
+    function handleSearch(event) {
+        setSearch(event.target.value);
+    }
+
+    function submitSearch(event) {
+        event.preventDefault();
+        console.log(search);
+    }
+
     return (
-        <div className="jumbotron">
-            <div className="container">
-                <h1 className="display-4">Welcome to Infact!</h1>
-                <p className="lead">The job board for contract and temporary work for Freelancers</p>
+        <div>
+            <div className="jumbotron">
+                <div className="container">
+                    <h1 className="display-4">Welcome to Infact!</h1>
+                    <p className="lead">The job board for contract and temporary work for Freelancers</p>
+                </div>
             </div>
+            <SearchBar handleSearch={handleSearch} submitSearch={submitSearch}/>
         </div>
     )
 }
