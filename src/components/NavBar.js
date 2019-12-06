a6728b707789c96cb52e5d58d32fb969ae157889
Alex Khoa Le <leduykhoa@hotmail.ca>
2019-12-05 20:15:16 -0500

    Initialized page structure, created collapsible navbar

---
+++ src/components/NavBar.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import Navbar from "react-bootstrap/Navbar";
+import { Link } from "react-router-dom";
+
+function NavBar() {
+  return (
+    <Navbar sticky="top" bg="light" expand="md">
+      <Link to="/" className="navbar-brand">Infact</Link>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <ul className="navbar-nav ml-auto">
+          <li className="nav-item active">
+            <Link to="/" className="nav-link">Home</Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/profile" className="nav-link">Profile</Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/signup" className="nav-link">Sign Up</Link>
+          </li>
+        </ul>
+      </Navbar.Collapse>
+    </Navbar>
+  )
+}
+
+export default NavBar;
---
+++ src/pages/Landing.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Landing() {
+    return (
+        <div className="jumbotron">
+            <div className="container">
+                <h1 className="display-4">Welcome to Infact!</h1>
+                <p className="lead">The job board for contract and temporary work for Freelancers</p>
+            </div>
+        </div>
+    )
+}
+
+export default Landing;
