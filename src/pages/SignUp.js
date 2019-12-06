c6cfc12d705bc4a31cc4f2005b3d9384c45645e8
Alex Khoa Le <leduykhoa@hotmail.ca>
2019-12-06 11:16:32 -0500

    Created sign up page base. Handles input and submission

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,9 +1,85 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+let warning = 0;
 
 function SignUp() {
+    const [formInput, setFormInput] = useState({
+        username: "",
+        firstName: "",
+        lastName: "",
+        password: ""
+    })
+
+    const [formState, setFormState] = useState({
+        userValidStyle: "none",
+        formValidStyle: "none"
+    })
+
+    function handleFormSubmit(event) {
+        event.preventDefault();
+        let confirmInput = Object.values(formInput).filter(value => { return value !== "" })
+        if (confirmInput.length === 4) {
+            console.log("All areas filled!")
+        } else {
+            clearTimeout(warning);
+            setFormState({ ...formState, formValidStyle: "block" })
+            warning = setTimeout(() => {
+                setFormState({ ...formState, formValidStyle: "none" })
+            }, 3000)
+        }
+    }
+
+    function handleInputChange(event) {
+        switch (event.target.id) {
+            case ("user"):
+                setFormInput({ ...formInput, username: event.target.value })
+                break;
+            case ("first-name"):
+                setFormInput({ ...formInput, firstName: event.target.value })
+                break;
+            case ("last-name"):
+                setFormInput({ ...formInput, lastName: event.target.value })
+                break;
+            case ("password"):
+                setFormInput({ ...formInput, password: event.target.value })
+                break;
+            default:
+                break;
+        }
+    }
+
     return (
-        <div>
-            
+        <div className="content row">
+            <div className="container col-xl-5 col-lg-6 col-md-8 col-sm-10">
+                <h1 className="text-center">Create an account</h1>
+                <form onSubmit={handleFormSubmit}>
+                    <div className="form-group">
+                        <label><i><strong> Username</strong></i></label>
+                        <input className="form-control form-control-lg" id="user" onChange={handleInputChange} />
+                        <small id="alertTaken" className="form-text text-danger" style={{ display: formState.userValidStyle }}>
+                            This username has already been taken!
+                    </small>
+                    </div>
+                    <div className="form-group">
+                        <label><i><strong> First Name</strong></i></label>
+                        <input className="form-control form-control-lg" id="first-name" onChange={handleInputChange} />
+                    </div>
+                    <div className="form-group">
+                        <label><i><strong> Last Name</strong></i></label>
+                        <input className="form-control form-control-lg" id="last-name" onChange={handleInputChange} />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="exampleInputPassword1"><i><strong> Password</strong></i></label>
+                        <input type="password" className="form-control form-control-lg" id="password" onChange={handleInputChange} />
+                    </div>
+                    <div className="button">
+                        <button className="btn btn-primary" style={{ marginBottom: "10px" }} id="createNewAccount">Submit</button>
+                    </div>
+                </form>
+                <div className="alert alert-danger" id="alertFailed" style={{ display: formState.formValidStyle }}>
+                    Please fill in missing information!
+                </div>
+            </div>
         </div>
     )
 
