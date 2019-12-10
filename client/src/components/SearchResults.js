5b6c7e763071fc1edd544e7c844c13dc8abef0d5
Alex Khoa Le <leduykhoa@hotmail.ca>
2019-12-10 16:25:03 -0500

    Search results returns cards for every result returned

--- client/src/components/SearchResults.js
+++ client/src/components/SearchResults.js
@@ -1,31 +1,36 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 
 function SearchResults(props) {
-	return (
-		<div className="row">
-			{props.results.map(post => {
-				return (
-					<div className="col-md-6 card">
-						<div className="row no-gutters">
-							<div class="col-md-4">
-								<img src={"https://" + post.company.logo.split("https://")[2]} class="card-img" alt="..." />
-							</div>
-							<div className="col-md-8">
-								<div className="card-body">
-									<h5 className="card-title">{post.title}</h5>
-									<p>{post.company.name}</p>
-									<small class="text-muted">Posted {post.post_date}</small>
-									<p>
-										{post.category.name} - {post.type.name}
-									</p>
-								</div>
-							</div>
-						</div>
-					</div>
-				)
-			})}
-		</div>
-	)
+  return (
+    <div className="row">
+      {props.results.map(post => {
+        let time = post.post_date.split(" ")[1]
+        return (
+          <div className="col-md-6 card">
+            <div className="row no-gutters">
+              <div className="col-md-4">
+                {/*The url needs to be split here since they smooshed two urls together*/}
+                <img src={"https://" + post.company.logo.split("https://")[2]} className="card-img" alt="..." />
+              </div>
+              <div className="col-md-8">
+                <div className="card-body">
+                  <Link className="card-title" to={"/post/" + post.id}>
+                    <h5>{post.title}</h5>
+                  </Link>
+                  <p>{post.company.name}</p>
+                  <small className="text-muted">Posted {post.post_date.split(" ")[0]} {Number(time.split(":")[0]) > 12 ? ((Number(time.split(":")[0]) - 12) + ":" + time.split(":")[1] + " PM").padStart(8, '0') : time.slice(0, 5) + " AM"}</small>
+                  <p>
+                    {post.category.name} - {post.type.name}
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        )
+      })}
+    </div>
+  )
 }
 
 export default SearchResults;
