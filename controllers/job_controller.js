be185e6fea9bd584d182852c543aae0074203df4
Alex Khoa Le <leduykhoa@hotmail.ca>
2019-12-10 16:25:47 -0500

    Created route to get details of one job, changed root get to catch all that returns index.html

--- controllers/job_controller.js
+++ controllers/job_controller.js
@@ -1,20 +1,28 @@
 const express = require("express");
 const axios = require("axios");
+const path = require("path");
 // Import the test module to access database functions
 // const model = require("../models/test_models.js");
 
 // The router is like using app = express(), where the server is defined in another file
 let router = express.Router();
 
-router.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
-
 router.get("/search/:search", async (req, res) => {
     let queryURL = `https://authenticjobs.com/api/?format=json&api_key=${process.env.AJ_API}&method=aj.jobs.search&keywords=${req.params.search}&perpage=10`
     let results = await axios.get(queryURL)
     res.send(results.data.listings.listing);
 })
+
+router.get("/api/post/:id", async (req, res) => {
+    let queryURL = `https://authenticjobs.com/api/?format=json&api_key=${process.env.AJ_API}&method=aj.jobs.get&id=${req.params.id}`
+    let results = await axios.get(queryURL)
+    console.log(results.data)
+    res.send(results.data);
+})
+
+router.get("/*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
 
 // router.get("/api/posts", (req, res) => {
 //     model.selectAll(result => {
