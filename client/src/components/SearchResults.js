1eac4d52d4670dd043e59cc7f9e0f26fb9f5168f
Alex Khoa Le <leduykhoa@hotmail.ca>
2019-12-09 21:17:52 -0500

    SearchResults now creates a card for every result returned by the API

--- client/src/components/SearchResults.js
+++ client/src/components/SearchResults.js
@@ -1,17 +1,31 @@
 import React from 'react';
 
 function SearchResults(props) {
-    return (
-        <div className="container">
-            {props.results.map(post => {
-                return (
-                    <div>
-                        {post.title}
-                    </div>
-                )
-            })}
-        </div>
-    )
+	return (
+		<div className="row">
+			{props.results.map(post => {
+				return (
+					<div className="col-md-6 card">
+						<div className="row no-gutters">
+							<div class="col-md-4">
+								<img src={"https://" + post.company.logo.split("https://")[2]} class="card-img" alt="..." />
+							</div>
+							<div className="col-md-8">
+								<div className="card-body">
+									<h5 className="card-title">{post.title}</h5>
+									<p>{post.company.name}</p>
+									<small class="text-muted">Posted {post.post_date}</small>
+									<p>
+										{post.category.name} - {post.type.name}
+									</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				)
+			})}
+		</div>
+	)
 }
 
 export default SearchResults;
