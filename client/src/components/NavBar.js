dae85b38a471a09bb62cafae620ab9eeb711779f
Alex Khoa Le <leduykhoa@hotmail.ca>
2019-12-11 13:12:55 -0500

    Navbar Home links to the search page instead of hero page

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -5,12 +5,12 @@ import { Link, NavLink } from "react-router-dom";
 function NavBar() {
   return (
     <Navbar sticky="top" bg="light" expand="md">
-      <Link to="/" className="navbar-brand">Working Title</Link>
+      <Link to="/home" className="navbar-brand">Working Title</Link>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <ul className="navbar-nav ml-auto">
           <li className="nav-item">
-            <NavLink exact to="/" className="nav-link" activeClassName="active">Home</NavLink>
+            <NavLink exact to="/home" className="nav-link" activeClassName="active">Home</NavLink>
           </li>
           <li className="nav-item">
             <NavLink to="/profile" className="nav-link" activeClassName="active">Profile</NavLink>
