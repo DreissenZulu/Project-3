51378de3f2289788218cc18fca5ef72d3fd34c71
Alex Khoa Le <leduykhoa@hotmail.ca>
2019-12-12 21:57:19 -0500

    Set up ajax call for user info. Moved profile JSX to component

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,12 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import ProfilePage from '../components/ProfilePage';
+// import InitializeUser from 'InitializeUser';
 
 function Profile() {
     const [profileInfo, setProfileInfo] = useState("")
-    let profileID = window.location.pathname.split("/")[1]
+    let profileID = window.location.pathname.split("/")[2]
+    // let profileID = window.localStorage.getItem('currUser');
+
+
 
     useEffect(() => {
-
         if (profileID !== undefined) {
             fetchUser()
         }
@@ -20,7 +24,7 @@ function Profile() {
         if (profileInfo === "") {
             try {
                 let profileData = await axios.get(`/api/user/${profileID}`)
-                setProfileInfo(profileData.data);
+                setProfileInfo(profileData.data[0]);
             } catch (err) {
                 console.log(err)
             }
@@ -28,43 +32,7 @@ function Profile() {
     }
 
     return (
-        <div>
-            <div className="container">
-                <div className="row d-flex">
-                    <div className="col-md-4 bg-info text-light" >
-                        <div style={{ textAlign: "center" }}>
-                            <img className="rounded-pf-image" src="https://randomuser.me/api/portraits/men/87.jpg" alt="random user" />
-                            <h3>
-                                <i className="fas fa-star"></i>
-                                <i className="fas fa-star"></i>
-                                <i className="fas fa-star"></i>
-                                <i className="fas fa-star"></i>
-                                <i className="far fa-star"></i>
-                            </h3>
-                        </div>
-                        <div>
-                            <h1>{`${profileInfo.firstName} ${profileInfo.lastName}`}</h1>
-                            <p><i>Freelance Web Developer</i></p>
-                            <p className="profile-bio">{profileInfo.bio}</p>
-                        </div>
-                    </div>
-                    <div className="col-md-8">
-                        <div className="row d-flex justify-content-center text-center">
-                            <div className="col-md-4">
-                            {profileInfo.email}
-                            </div>
-                            <div className="col-md-4">
-                            {`${profileInfo.city} ${profileInfo.country}`}
-                            </div>
-                            <div className="col-md-4">
-                                (502)-106-2694
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-            </div>
-        </div>
+        <ProfilePage profileInfo={profileInfo}/>
     )
 
 }
