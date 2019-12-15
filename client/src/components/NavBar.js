c885f9f5f6379049f697979ff7b10104820d9b0f
Alex Khoa Le <leduykhoa@hotmail.ca>
2019-12-15 17:43:25 -0500

    Navbar contents are adjusted based on whether the user is logged in or logged out

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -3,17 +3,28 @@ import Navbar from "react-bootstrap/Navbar";
 import { Link, NavLink } from "react-router-dom";
 
 function NavBar() {
-  return (
-    <Navbar sticky="top" bg="light" expand="md">
-      <Link to="/home" className="navbar-brand">Working Title</Link>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
+  let currUser = JSON.parse(localStorage.getItem("currUser"));
+
+  function NavOptions() {
+    if (currUser !== null) {
+      return (
         <ul className="navbar-nav ml-auto">
           <li className="nav-item">
             <NavLink exact to="/home" className="nav-link" activeClassName="active">Home</NavLink>
           </li>
           <li className="nav-item">
-            <NavLink to="/profile" className="nav-link" activeClassName="active">Profile</NavLink>
+            <NavLink to={`/profile/${currUser.id}`} className="nav-link" activeClassName="active">Profile</NavLink>
+          </li>
+          <li className="nav-item">
+            <a href="/home" className="nav-link" onClick={signOut} activeClassName="active">Sign Out</a>
+          </li>
+        </ul>
+      )
+    } else {
+      return (
+        <ul className="navbar-nav ml-auto">
+          <li className="nav-item">
+            <NavLink exact to="/home" className="nav-link" activeClassName="active">Home</NavLink>
           </li>
           <li className="nav-item">
             <NavLink to="/signup" className="nav-link" activeClassName="active">Sign Up</NavLink>
@@ -22,6 +33,21 @@ function NavBar() {
             <NavLink to="/login" className="nav-link" activeClassName="active">Log In</NavLink>
           </li>
         </ul>
+      )
+    }
+  }
+
+  function signOut() {
+    localStorage.removeItem("currUser");
+    window.location.pathname = "/home";
+  }
+
+  return (
+    <Navbar sticky="top" bg="info" variant="dark" expand="md">
+      <Link to="/home" className="navbar-brand">YuMi Jobs</Link>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <NavOptions />
       </Navbar.Collapse>
     </Navbar>
   )
