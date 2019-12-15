8c356556472a117c404f242a8ae938db66e4ed4b
Alex Khoa Le <leduykhoa@hotmail.ca>
2019-12-15 17:43:40 -0500

    Changed hero text to match our new name

--- client/src/pages/Hero.js
+++ client/src/pages/Hero.js
@@ -5,8 +5,8 @@ function Hero() {
     return (
         <section className="hero noselect">
             <div className="hero-inner">
-                <h1 className="text-light">Welcome to Working Title!</h1>
-                <h3 className="text-light">The Job Board by Freelancers, for Freelancers</h3>
+                <h1 className="text-light">Welcome to YuMi Jobs!</h1>
+                <h3 className="text-light">The Job Board by Mi, for Yu</h3>
                 <Link to="/home"><button className="btn btn-primary">Get Started!</button></Link>
             </div>
         </section>
