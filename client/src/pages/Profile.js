6f9f2fd406c59253e52307e1370f32fd0fa110d2
Alex Khoa Le <leduykhoa@hotmail.ca>
2019-12-14 20:00:35 -0500

    Profile page checks if there is nothing in local storage

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,15 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import ProfilePage from '../components/ProfilePage';
-// import InitializeUser from 'InitializeUser';
 
 function Profile() {
     let profileID = window.location.pathname.split("/")[2]
-
+    let currUser = JSON.parse(localStorage.getItem('currUser'));
     // Redirects the user to set up their profile if they have no assigned role yet
-    if ((profileID == JSON.parse(localStorage.getItem('currUser')).id) && (JSON.parse(localStorage.getItem('currUser')).role === null)) {
-        window.location.pathname = "/setup";
+    if (currUser !== null) {
+        if ((profileID == currUser.id) && (currUser.role === null)) {
+            window.location.pathname = "/setup";
+        }
     }
+
     const [profileInfo, setProfileInfo] = useState("")
     // let profileID = window.localStorage.getItem('currUser');
 
