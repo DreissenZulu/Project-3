1f2ace2a94f976c9c121c8459c4a4ecfe000fece
Idris <idrisadebisi@live.com>
2019-12-15 16:38:33 -0500

    adding phone number

--- seed.sql
+++ seed.sql
@@ -1,7 +1,7 @@
-INSERT INTO user (firstName, lastName, email, password, bio, city, country, role, image_url, link1, link2, link3) VALUES 
-('Richard', 'Koln', 'rkoln@test.com', 'rkoln','Proffessional foodie, looking to put my skills to greater use', 'Toronto', 'Canada', 'job seeker', 'www.images.com', 'youtube.com','linkedin.com','twitter.com'),
-('Donald', 'Gaffer', 'donald@test.com', 'password','Front End web developer, with a great eye for design', 'New York', 'USA', 'job seeker', 'www.images.com', 'youtube.com','linkedin.com','twitter.com'),
-('Chris', 'Benoit', 'benoit@test.com', 'domicide','Wrestler, Fighter, Macho man', 'Nebraska', 'USA', 'Talent hunter', 'www.images.com', 'youtube.com','linkedin.com','twitter.com');
+INSERT INTO user (firstName, lastName, email, phoneNumber, password, bio, city, country, role, image_url, link1, link2, link3) VALUES 
+('Richard', 'Koln', 'rkoln@test.com', '07908236542', 'rkoln','Proffessional foodie, looking to put my skills to greater use', 'Toronto', 'Canada', 'job seeker', 'www.images.com', 'youtube.com','linkedin.com','twitter.com'),
+('Donald', 'Gaffer', 'donald@test.com','6472426789', 'password','Front End web developer, with a great eye for design', 'New York', 'USA', 'job seeker', 'www.images.com', 'youtube.com','linkedin.com','twitter.com'),
+('Chris', 'Benoit', 'benoit@test.com', '07026723465', 'domicide','Wrestler, Fighter, Macho man', 'Nebraska', 'USA', 'Talent hunter', 'www.images.com', 'youtube.com','linkedin.com','twitter.com');
 
 INSERT INTO savedjobs (jobtitle, jobid, userid) VALUES 
 ('Full stack web developer',100, 2),('Junior Chef',101, 1), ('WWE Diva',102, 3), ('Food Taster',103, 1), ('Web designer',104, 2);
