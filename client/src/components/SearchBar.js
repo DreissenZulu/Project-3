d038ac8cb525e1552148df1ab00a5eff5a25d839
Alex Khoa Le <leduykhoa@hotmail.ca>
2019-12-14 19:57:37 -0500

    Changed placeholder text for user search

--- client/src/components/SearchBar.js
+++ client/src/components/SearchBar.js
@@ -25,7 +25,7 @@ function SearchBar(props) {
               <input className="form-control" name="search" onChange={props.handleSearch} type="search" placeholder="Search for a user or a role" autoComplete="off" aria-label="Search" />
             </div>
             <div className="form-group col-md-6">
-              <input className="form-control" name="location" onChange={props.handleSearch} type="search" placeholder="Optional: add a location (eg. New York)" autoComplete="off" aria-label="Location" />
+              <input className="form-control" name="location" onChange={props.handleSearch} type="search" placeholder="Search by location (eg. Toronto)" autoComplete="off" aria-label="Location" />
             </div>
           </div>
 
