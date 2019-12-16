c4f5698e1963f3e92160b547a26e85f412248a3c
Alex Khoa Le <leduykhoa@hotmail.ca>
2019-12-16 10:35:40 -0500

    Added animation to hero text

--- client/src/pages/Hero.js
+++ client/src/pages/Hero.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 function Hero() {
     return (
         <section className="hero noselect">
-            <div className="hero-inner">
+            <div className="hero-inner fadeIn">
                 <h1 className="text-light">Welcome to YuMi Jobs!</h1>
                 <h3 className="text-light">The Job Board by Mi, for Yu</h3>
                 <Link to="/home"><button className="btn btn-primary">Get Started!</button></Link>
