30f92220e61d2a4fe58721e1a80207e0e8fa8812
Idris <idrisadebisi@live.com>
2019-12-15 21:53:48 -0500

    connecting users

--- client/src/components/SearchResults.js
+++ client/src/components/SearchResults.js
@@ -1,8 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import { Link } from "react-router-dom";
 import userPlaceholder from "../assets/Portrait_Placeholder.png";
 
 function SearchResults(props) {
+
+  let userID = JSON.parse(localStorage.getItem("currUser")).id;
+
+  const [added, setAdded] = useState( "init" )
+
+  useEffect(() => {
+    checkAdded();
+})
+
+useEffect(() => {
+  checkAdded();
+}, [added])
+
+async function checkAdded(){
+  if (added === "init") {
+  let result = await axios.get(`/api/people/${userID}`)
+            setAdded(result.data.map(idObj => idObj.connectedid));
+  }
+}
+
+
+
+  function addUser(event){
+        let sendObj = {}
+        sendObj.userId = JSON.parse(localStorage.getItem("currUser")).id;
+        sendObj.addedUser = Number(event.target.value);
+        let url = '/adduser';
+        axios.post(url, sendObj);
+
+  }
+
+
+
   if (props.results === "none") {
     return (
       <div>
@@ -19,7 +53,7 @@ function SearchResults(props) {
               <div className="row no-gutters">
                 <div className="col-xs-2">
                   {/*The url needs to be split here since they smooshed two urls together*/}
-                  <img style={{maxHeight: "128px"}} src={"https://" + post.company.logo.split("https://")[2]} className="card-img" alt="..." />
+                  <img style={{maxHeight: "128px", maxWidth: "128px"}} src={"https://" + post.company.logo.split("https://")[2]} className="card-img" alt="..." />
                 </div>
                 <div className="col-xs-10">
                   <div className="card-body">
@@ -47,7 +81,7 @@ function SearchResults(props) {
             <div className="col-lg-6 card">
               <div className="row no-gutters">
                 <div className="col-xs-2">
-                  <img style={{maxHeight: "128px"}} src={user.image_url ? user.image_url : userPlaceholder} className="card-img" alt="User image" />
+                  <img style={{maxHeight: "128px", maxWidth: "128px"}} src={user.image_url ? user.image_url : userPlaceholder} className="card-img" alt="User image" />
                 </div>
                 <div className="col-xs-9" style={{marginRight: "auto"}}>
                   <div className="card-body">
@@ -58,7 +92,7 @@ function SearchResults(props) {
                   </div>
                 </div>
                 <div className="col-xs-1">
-                  <button className="btn btn-primary" style={{marginTop: "10px", marginRight: "10px"}}><i class="fas fa-plus" aria-disabled="false"></i></button>
+                  <button className={added.includes(user.id) ? "btn btn-secondary disabled" : "btn btn-primary"} disabled={added.includes(user.id)} value = {user.id} onClick={addUser} style={{marginTop: "10px", marginRight: "10px"}}><i class="fas fa-plus" aria-disabled="false"></i></button>
                 </div>
               </div>
             </div>
