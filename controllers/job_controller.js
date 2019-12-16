bbf1b28731e2be1978421b191ad1d5bfb9650662
Alex Khoa Le <leduykhoa@hotmail.ca>
2019-12-15 21:58:57 -0500

    Changed search to add Indeed scraper

--- controllers/job_controller.js
+++ controllers/job_controller.js
@@ -2,17 +2,33 @@ const express = require("express");
 const axios = require("axios");
 const path = require("path");
 const orm = require('../config/orm');
-const connection = require("../config/connection.js");
+const indeed = require('indeed-scraper');
 // Import the test module to access database functions
 // const model = require("../models/test_models.js");
 
 // The router is like using app = express(), where the server is defined in another file
 let router = express.Router();
 
-router.get("/search/:search", async (req, res) => {
-    let queryURL = `https://authenticjobs.com/api/?format=json&api_key=cb2d8eec4216145cb45cf496d7b00323&method=aj.jobs.search&keywords=${req.params.search}&perpage=10`
+router.get("/search/:search/:location?", async (req, res) => {
+    const search = req.params.search
+    const location = req.params.location
+    
+    const queryOptions = {
+        host: 'www.indeed.com',
+        query: req.params.search,
+        city: req.params.location,
+        radius: '25',
+        level: 'entry_level',
+        jobType: 'freelance',
+        maxAge: '7',
+        sort: 'date',
+        limit: 10
+    };
+    let searchParams = `${search ? "&keywords="+search : ""}${location ? "&location="+location : ""}`
+    let queryURL = `https://authenticjobs.com/api/?format=json&api_key=cb2d8eec4216145cb45cf496d7b00323&method=aj.jobs.search${searchParams}&perpage=10`
     let results = await axios.get(queryURL)
-    console.log(results)
+    let indeedResults = await indeed.query(queryOptions)
+    console.log(indeedResults)
     res.send(results.data.listings.listing);
 })
 
@@ -101,7 +117,7 @@ router.post("/register", async (req, res) => {
     });
 });
 
-router.put("/user", async(req, res)=>{
+router.put("/user", async (req, res) => {
     let id = req.body.id;
     let name = req.body.name;
     let firstName = name.split(' ')[0];
@@ -109,7 +125,7 @@ router.put("/user", async(req, res)=>{
     let title = req.body.title;
 
     let location = req.body.location;
-    if(location.indexOf(',') > -1){
+    if (location.indexOf(',') > -1) {
         let index = location.indexOf(',');
         let l1 = location.substring(0, index);
         let l2 = location.slice(index + 1);
@@ -121,12 +137,12 @@ router.put("/user", async(req, res)=>{
     let phoneNumber = req.body.number;
     let bio = req.body.bio;
 
-    try{
+    try {
         let result = await orm.updateData(
             'user',
             `firstName="${firstName}", lastName="${lastName}", title="${title}", city="${city}", country="${country}", phoneNumber="${phoneNumber}", bio="${bio}"`,
             `id=${id}`,
-            (result)=>{
+            (result) => {
                 res.send(result);
             }
         );
